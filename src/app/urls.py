e7233f8781bfffb9b41d7492f7e67a56a5f775ed
Dave Jensen <davidj@nextravel.com>
2021-03-15 22:58:31 -0700

    enable openapi schema generation and documentation

--- src/app/urls.py
+++ src/app/urls.py
@@ -15,9 +15,34 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.renderers import JSONOpenAPIRenderer, OpenAPIRenderer
+from rest_framework.schemas import get_schema_view
+
+title = "Recipes API"
+version = "1.0.0"
+
+# TODO: if you can specify multiple renderers, shouldn't there be a way to re-use the result of get_schema_view?
+schema_json = get_schema_view(
+    title=title,
+    version=version,
+    renderer_classes=[JSONOpenAPIRenderer],
+)
+
+schema_yaml = get_schema_view(
+    title=title,
+    version=version,
+    renderer_classes=[OpenAPIRenderer],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
+    path('schema.json', schema_json),
+    path('schema.yaml', schema_yaml, name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ))
     # path('', recipes.urls)
 ]
