ed93e8696b8e6967d183e38cc49dc439070e6afa
Dave Jensen <davidj@nextravel.com>
2021-03-15 21:59:46 -0700

    add views to create and list recipes

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Recipe
+from .serializers import RecipeSerializer
+
+
+class RecipeList(APIView):
+    """ List all recipes or create a new recipe"""
+
+    def get(self, request: Request, format=None) -> Response:
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request: Request, format=None) -> Response:
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
