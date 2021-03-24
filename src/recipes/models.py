ebaf5da1df2ccb826f26edbd634c74d09598df80
Dave Jensen <davidj@nextravel.com>
2021-03-24 16:10:00 -0700

    add ingredient model

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Recipe(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=255, blank=False)
     description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name="ingredients", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
