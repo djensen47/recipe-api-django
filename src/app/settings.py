1e65c533d109ada86eae5358d11748f167bdf2b1
Dave Jensen <davidj@nextravel.com>
2021-03-24 17:13:34 -0700

    add drf-spectacular to dependencies

--- src/app/settings.py
+++ src/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'recipes.apps.RecipesConfig',
 ]
 
@@ -123,6 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DRF Configuration
+REST_FRAMEWORK = {
+  'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 # Whether to append trailing slashes to URLs.
 APPEND_SLASH = False
